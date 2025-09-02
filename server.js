011b4672e604448827d7110e7b4442a1e95944dc
Nakura001 <gabrieljeanmarkos2@gmail.com>
2025-09-01 21:51:25 -0400

    const hash terminada

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  /*Store hash in your db*/
-  console.log(hash + "<==== this is the hash bcrypt");
+app.get('/', (req, res) => {
+  res.send("Hello World");
 });
-
-bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-  console.log(res, "<=== res here");
-  /*res == true or false*/
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+    /*res == true or false*/
+  });
 });
 
 //END_ASYNC
